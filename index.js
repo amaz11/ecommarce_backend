a01defc250f1ffa7a3391d23c52180f459685cf3
K M Amaz Uddin Shaon <k.m.shaonred@gmail.com>
2023-08-05 13:55:42 +0600

    cors2

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json({ limit: "10mb" }));
 // );
 app.use(cors());
 app.use(sameSiteCookieMiddleware());
-// app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 // app.use(
 //   cookieSession({
 //     name: "__session",
