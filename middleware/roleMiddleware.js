71a89ac46da130dd567f46e3b2e83640bd0e26b8
K M Amaz Uddin Shaon <k.m.shaonred@gmail.com>
2023-08-05 20:27:41 +0600

    jwt

--- middleware/roleMiddleware.js
+++ middleware/roleMiddleware.js
@@ -1,11 +1,14 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res, next) => {
-  const token = req.cookies.jwt;
+  let token = req.headers.authorization;
+
   if (!token) {
     return res.status(401).json({ error: "Not athurize" });
   }
   try {
+    token = token.split("Bearer ")[1];
+
     const decode = jwt.verify(token, process.env.JWT_SECRET);
     if (!decode) {
       return res.json({ error: "Invalid Token" });
