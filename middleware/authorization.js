04482bc209f4e8e05e04e87f6a129c65e46a6992
K M Amaz Uddin Shaon <k.m.shaonred@gmail.com>
2023-08-06 19:26:06 +0600

    login change

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res, next) => {
   const token = req.cookies.jwt;
+  console.log(token);
   if (!token) {
     return res.status(401).json({ error: "Not athurize" });
   }
